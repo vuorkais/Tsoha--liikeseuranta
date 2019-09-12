54db6a8c83e35bbdb13ceae65c7c3149bbd67cb6
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-12 16:53:29 +0300

    Update __init__.py

--- application/voimistelijat/__init__.py
+++ application/voimistelijat/__init__.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///voimistelijat.db"
 app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 
