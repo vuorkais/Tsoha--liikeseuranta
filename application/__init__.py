bf212ff68f6ca764b898ff74aaf9a34dcd0d3b05
Kaisla <kaisla.vuorenmaa@gmail.com>
2019-09-12 16:20:35 +0300

    luotu tiedostoja sql-tietokantasovellusta varten

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,21 @@
+# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
+# Tuodaan SQLAlchemy käyttöön
+from flask_sqlalchemy import SQLAlchemy
+# Käytetään gymnasts.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
+# samassa paikassa
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gymnasts.db"
+# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+app.config["SQLALCHEMY_ECHO"] = True
+
+# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+db = SQLAlchemy(app)
+
+# Luetaan kansiosta application tiedoston views sisältö
 from application import views
+
+# Luodaan lopulta tarvittavat tietokantataulut
+db.create_all()
