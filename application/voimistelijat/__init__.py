be01246a40365aeac288ba3be916358a8e6367e4
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-12 16:35:55 +0300

    Update __init__.py

--- application/voimistelijat/__init__.py
+++ application/voimistelijat/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gymnasts.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///voimistelijat.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
 from application import views
 
-from application.gymnasts import models
+from application.voimistelijat import models
 
 db.create_all()
