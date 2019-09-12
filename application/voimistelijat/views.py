7c0f9a67539a10073834eb7aeedebe6af0103ab8
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-12 18:40:15 +0300

    Update views.py

--- application/voimistelijat/views.py
+++ application/voimistelijat/views.py
@@ -1,12 +1,13 @@
-from application import app
+from application import app, db
 from flask import render_template, request
+from application.voimistelijat.models import Voimistelija
 
 @app.route("/voimistelijat/uusi/")
 def voimistelijat_form():
     return render_template("voimistelijat/uusi.html")
 
 @app.route("/voimistelijat/", methods=["POST"])
-def tasks_create():
+def voimistelijat_create():
     t = Task(request.form.get("nimi"))
 
     db.session().add(t)
