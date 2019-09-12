3b5c360d35c26db7fb405a0c341ac460750805ea
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-12 18:25:09 +0300

    Update views.py

--- application/views.py
+++ application/views.py
@@ -1,6 +1,12 @@
-from flask import render_template
 from application import app
+from flask import render_template, request
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/voimistelijat/uusi/")
+def tasks_form():
+    return render_template("voimistelijat/uusi.html")
+
+@app.route("/voimistelijat/", methods=["POST"])
+def tasks_create():
+    print(request.form.get("nimi"))
+  
+    return "hello world!"
