c4d6fbbbefd628a01e8588305a2e1813e68b5693
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-12 17:18:58 +0300

    Update models.py

--- application/voimistelijat/models.py
+++ application/voimistelijat/models.py
@@ -1,14 +1,14 @@
 from application import db
 
 class Voimistelija(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    	onupdate=db.func.current_timestamp())
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
 
-	nimi = db.Column(db.String(144), nullable=False)
-	ryhma = db.Column(db.String(144), nullable=False)
+    nimi = db.Column(db.String(144), nullable=False)
+    ryhma = db.Column(db.String(144), nullable=False)
 	
-	def __init__(self, name):
-		self.nimi = name
-		self.ryhma = ryhma
+    def __init__(self, name):
+	self.nimi = name
+	self.ryhma = ryhma
