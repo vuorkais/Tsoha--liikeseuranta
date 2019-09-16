661e998ee45c18761712d05f56d3b2d434535e0a
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-16 09:46:36 +0300

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -15,7 +15,7 @@ app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 # Luetaan kansiosta application tiedoston views sisältö
-from application import views
+# from application import views
 
 # Luetaan kansiosta application tiedoston models sisältö
 from application.voimistelijat import models
