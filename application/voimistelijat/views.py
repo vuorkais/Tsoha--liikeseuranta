ad6c15a726882de0d23fa6c0d7188f3033742917
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-16 09:55:32 +0300

    Create views.py

---
+++ application/voimistelijat/views.py
@@ -0,0 +1,20 @@
+from application import app, db
+from flask import render_template, redirect, request, url_for
+from application.voimistelijat.models import Voimistelija
+
+@app.route("/voimistelijat", methods=["GET"])
+def voimistelijat_index():
+    return render_template("voimistelijat/list.html", voimistelijat = Voimistelija.query.all())
+
+@app.route("/voimistelijat/uusi/")
+def voimistelijat_form():
+    return render_template("voimistelijat/uusi.html")
+
+@app.route("/voimistelijat/", methods=["POST"])
+def voimistelijat_create():
+    v = Voimistelija(request.form.get("nimi"))
+
+    db.session().add(v)
+    db.session().commit()
+    
+    return redirect(url_for("voimistelijat_index"))
