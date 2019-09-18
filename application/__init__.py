23e5758193789bf6501227f1bbda7efa1db1ae11
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-18 13:43:15 +0300

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -14,12 +14,11 @@ app.config["SQLALCHEMY_ECHO"] = True
 # Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
 
-# Luetaan kansiosta application tiedoston views sisältö
+# Luetaan kansiosta application tiedostojen models ja views sisältö
 from application import views
-
-# Luetaan kansiosta application tiedoston models sisältö
 from application.voimistelijat import models
 from application.voimistelijat import views
+from application.auth import models 
 
 # Luodaan lopulta tarvittavat tietokantataulut
 db.create_all()
