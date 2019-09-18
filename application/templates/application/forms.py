7ac1f155004ef6dea945bfcdd1a05a8f1e6ac738
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-18 14:27:47 +0300

    Delete forms.py

--- application/templates/application/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import VoimistelijaForm
-from wtforms import PasswordField, StringField
-  
-class LoginForm(FlaskForm):
-    username = StringField("Käyttäjänimi")
-    password = PasswordField("Salasana")
-  
-    class Meta:
-        csrf = False
