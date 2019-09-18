babfce18ae486384ce03d188d02ced58490e83b9
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-18 14:59:10 +0300

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -21,6 +21,22 @@ from application.voimistelijat import views
 from application.auth import models 
 from application.auth import views 
 
+# kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Ole hyvä ja kirjaudu."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 # Luodaan lopulta tarvittavat tietokantataulut
 db.create_all()
 
