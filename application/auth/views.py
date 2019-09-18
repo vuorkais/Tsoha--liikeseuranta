c0e5bcd802ffd0a67fb4b8010c3b39152e1498ad
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-18 14:37:11 +0300

    Create views.py

---
+++ application/auth/views.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, redirect, url_for
+
+from application import app
+from application.auth.models import User
+from application.auth.forms import LoginForm
+
+@app.route("/auth/login", methods = ["GET", "POST"])
+def auth_login():
+    if request.method == "GET":
+        return render_template("auth/loginform.html", form = LoginForm())
+
+    form = LoginForm(request.form)
+    # mahdolliset validoinnit
+
+    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/loginform.html", form = form,
+                               error = "Käyttäjänimi tai salasana väärä!")
+
+
+    print("Käyttäjä " + user.name + " tunnistettiin")
+    return redirect(url_for("index")) 
