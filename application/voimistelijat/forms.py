bccb3fd027549e4201486e33ae4bcb36651e0630
vuorkais <48551591+vuorkais@users.noreply.github.com>
2019-09-18 13:32:57 +0300

    Update forms.py

--- application/voimistelijat/forms.py
+++ application/voimistelijat/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
 class VoimistelijaForm(FlaskForm):
-    nimi = StringField("Voimistelijan nimi", [validators.Length(min=2)])
-    ryhma = StringField("Voimistelijan ryhmä", [validators.Length(min=2)])
+    nimi = StringField("Voimistelijan nimi", [validators.Length(min=2, max=144, message="syöte on väärän pituinen!")])
+    ryhma = StringField("Voimistelijan ryhmä", [validators.Length(min=2, max=144, message="syöte on väärän pituinen!")])
  
     class Meta:
         csrf = False
