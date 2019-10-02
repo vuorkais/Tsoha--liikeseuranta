467caed280c58bb818790d2e0d589ebec5cbebae
Kaisla <kaisla.vuorenmaa@gmail.com>
2019-10-02 20:46:35 +0300

    aloitettu ryhmälistasta voimistelijan lisäys

--- application/ryhmat/views.py
+++ application/ryhmat/views.py
@@ -35,9 +35,6 @@ def ryhmat_create():
 def ryhmat_remove(ryhma_id):
 
     r = Ryhma.query.get(ryhma_id)
-    if r.vastuuvalmentaja_id != current_user.id:
-        print("EI ole sinun ryhmä!")
-        return redirect(url_for("ryhmat_index"))
     
     db.session().delete(r)
     db.session().commit()
--- application/voimistelijat/views.py
+++ application/voimistelijat/views.py
@@ -4,31 +4,13 @@ from flask_login import login_required, current_user
 from application import app, db
 from application.voimistelijat.models import Voimistelija
 from application.voimistelijat.forms import VoimistelijaForm
+from application.ryhmat.models import Ryhma
+from application.ryhmat.forms import RyhmaForm
+from application.liikkeet.models import Liike
 
 @app.route("/voimistelijat", methods=["GET"])
 def voimistelijat_index():
     return render_template("voimistelijat/list.html", voimistelijat = Voimistelija.query.all())
-
-@app.route("/voimistelijat/uusi/")
-@login_required
-def voimistelijat_form():
-    return render_template("voimistelijat/uusi.html", form = VoimistelijaForm())
-
-@app.route("/voimistelijat/", methods=["POST"])
-@login_required
-def voimistelijat_create():
-    form = VoimistelijaForm(request.form)
-    
-    if not form.validate():
-        return render_template("voimistelijat/uusi.html", form = form)
-    
-    v = Voimistelija(form.nimi.data)
-    v.vastuuvalmentaja_id = current_user.id
-    
-    db.session().add(v)
-    db.session().commit()
-    
-    return redirect(url_for("voimistelijat_index"))
 
 @app.route("/voimistelijat/<voimistelija_id>/remove", methods=["POST"])
 @login_required
@@ -39,4 +21,28 @@ def voimistelijat_remove(voimistelija_id):
     db.session().delete(v)
     db.session().commit()
   
+    return redirect(url_for("voimistelijat_index"))
+
+@app.route("/voimistelijat/lisays/")
+@login_required
+def voimistelijat_form():
+    return render_template("voimistelijat/uusi.html", form = VoimistelijaForm())
+
+@app.route("/ryhmat/lisataan", methods=["POST"])
+@login_required
+def voimistelijat_lisaa(ryhma_id):
+
+    form = VoimistelijaForm(request.form)
+    
+    if not form.validate():
+        return render_template("voimistelijat/uusi.html", form = form)
+    
+    v = Voimistelija(form.nimi.data)
+    v.vastuuvalmentaja_id = current_user.id
+    v.ryhma_id = 0
+    #Ryhma.query.get(ryhma_id)
+    
+    db.session().add(v)
+    db.session().commit()
+    
     return redirect(url_for("voimistelijat_index"))
