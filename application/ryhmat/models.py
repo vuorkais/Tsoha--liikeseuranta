1cf6e921e6c312db2a07ed6ccf106876a128f1d7
Kaisla <kaisla.vuorenmaa@gmail.com>
2019-10-03 19:47:21 +0300

    tietokantoja luodessa luodaan 'Ryhmättömät'-ryhmä

--- application/ryhmat/models.py
+++ application/ryhmat/models.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 class Ryhma(Base):
 
     ryhma = db.Column(db.String(144), nullable=False)
-
     vastuuvalmentaja_id = db.Column(db.Integer, db.ForeignKey('vastuuvalmentaja.id'), nullable=False)
 
-    def __init__(self, ryhma):
+    def __init__(self, ryhma, vastuuvalmentaja_id):
         self.ryhma = ryhma
+        self.vastuuvalmentaja_id = vastuuvalmentaja_id
