c71583782fa1c8a351bdbc18986518560fe6ed9c
Kaisla <kaisla.vuorenmaa@gmail.com>
2019-10-03 19:50:35 +0300

    Ryhmän luonti muutettu kommentoinniksi

--- application/__init__.py
+++ application/__init__.py
@@ -84,8 +84,8 @@ from application.ryhmat.models import Ryhma
 db.create_all()
 
 #Luodaan 'ryhmättömät' ryhmä
-Ryhma(ryhma='Ryhmättömät', vastuuvalmentaja_id=0)
-tyhja_ryhma = Ryhma(ryhma='Ryhmättömät', vastuuvalmentaja_id=0)
-db.session().add(tyhja_ryhma)
-db.session().commit()
+#Ryhma(ryhma='Ryhmättömät', vastuuvalmentaja_id=0)
+#tyhja_ryhma = Ryhma(ryhma='Ryhmättömät', vastuuvalmentaja_id=0)
+#db.session().add(tyhja_ryhma)
+#db.session().commit()
 
